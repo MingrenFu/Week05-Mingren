f555890f1ea7db65187d2c618d5fc50abb7bba3c
MingrenFu <mf3668@nyu.edu>
2023-02-21 16:37:27 -0500

    int

---
+++ Week-05 Demo/Week-05 Demo/Week_05_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week_05_DemoApp.swift
+//  Week-05 Demo
+//
+//  Created by Michael Fu on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Week_05_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
